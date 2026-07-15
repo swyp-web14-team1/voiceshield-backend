e7e8227b04081b5dd1e852b5189e49b3c994bf3d
recon3 <jch200325@naver.com>
2026-07-15 17:20:21 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/swyp/voiceshield/VoiceshieldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.voiceshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceshieldBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceshieldBackendApplication.class, args);
+	}
+
+}
